da5e214c1b0588e3cdf0312f0a599b7976741171
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2022-09-12 12:11:13 +0600

    fix: CORS error

--- cmd/ports/main.go
+++ cmd/ports/main.go
@@ -4,33 +4,41 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/handlers"
+	// "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 
 	"github.com/sifatulrabbi/ports/pkg/configs"
 	"github.com/sifatulrabbi/ports/pkg/controllers"
 	"github.com/sifatulrabbi/ports/pkg/utils"
 )
 
-func main() {
-	var (
-		r = mux.NewRouter()
-	)
-	configs.LoadENVs()
-	configs.ConnectDB()
-
+func registerRoutes(r *mux.Router) {
 	r.HandleFunc("/hello", controllers.HelloGET).Methods("GET")
 	r.HandleFunc("/hello", controllers.TestMongoDB).Methods("POST")
 	r.HandleFunc("/api/v1/auth/register", controllers.Register).Methods("POST")
 	r.HandleFunc("/api/v1/auth/signin", controllers.SignIn).Methods("POST")
 	r.HandleFunc("/api/v1/auth/accesstoken", controllers.GetAccessToken).Methods("GET")
 	r.HandleFunc("/api/v1/users/{username}", utils.AuthGuard(controllers.GetUserByUsername)).Methods("GET")
+}
 
-	originOk := handlers.AllowedOrigins([]string{"*"})
-	headersOk := handlers.AllowedHeaders([]string{"x-custom-header"})
-	methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
+func main() {
+	r := mux.NewRouter()
+	configs.LoadENVs()
+	configs.ConnectDB()
+	// Register all the routes
+	registerRoutes(r)
+	// Handling CORS
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:3000"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Content-Type", "Origin", "Authorization", "Host", "Accept", "User-Agent"},
+		Debug:          true,
+	})
+
 	log.Printf("Starting the server on port %v\n", configs.Globals.PORT)
-	if err := http.ListenAndServe(configs.Globals.PORT, handlers.CORS(originOk, headersOk, methodsOk)(r)); err != nil {
+	if err := http.ListenAndServe(configs.Globals.PORT, c.Handler(r)); err != nil {
 		log.Fatalln(err)
 	}
+
 }
